c07f22e91842fd9490f8a1d8ab5d8846518ab9a1
JericoCancel1 <86682959+JericoCancel1@users.noreply.github.com>
2023-08-26 23:25:43 -0400

    Add files via upload

---
+++ src/ex3.c
@@ -0,0 +1,49 @@
+#include "include/ex3.h"
+#include <math.h>
+
+void decode(char* cmd){
+    //TODO
+    const char *s;
+    for(s =cmd; *s; ++s);
+    int length = s- cmd;
+    double n = sqrt(length);
+
+    if(n != (int) n){
+        strcpy(cmd, "INVALID");
+    }
+    else{
+        char *temp = malloc(length +1);
+        int counter =0;
+        for(int i =0; i< (int) n; i++){
+            for(int j=0; j < (int) n; j++){
+                temp[counter] = cmd[j * (int)n+i];
+                counter++;
+            }
+        }
+        strcpy(cmd,temp);
+        free(temp);
+    }
+    printf("%s\n",cmd);
+}
+
+int main(void){
+    // Tests Variables
+    char cmd[MAX_CHARS];
+
+    //Test 1
+    strcpy(cmd, "WE OUE OUT LNGSAO H RWDN!");
+    decode(cmd); // Expected: WE ARE LOW ON DOUGHNUTS !
+    memset(cmd,0,MAX_CHARS);
+
+    //Test 2
+    strcpy(cmd, "S EFEM FNOCEDROE");
+    decode(cmd); // Expected: SEND MORE COFFEE
+    memset(cmd,0,MAX_CHARS);
+
+    //Test 3
+    strcpy(cmd, "CSERULES");
+    decode(cmd); // Expected: INVALID
+    memset(cmd,0,MAX_CHARS);
+
+    return 0;
+}
